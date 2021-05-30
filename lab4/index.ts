e3870e4e5e726a9c4087ee6043f715c70adf6460
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-05-30 17:49:42 +0200

    Laby 5 30.05.21

--- lab4/index.ts
+++ lab4/index.ts
@@ -1,8 +1,8 @@
-const addBTN = documentElementByID('add')
+const addBtn = document.getElementByID('add')
 
-addBTN.addEventListener('click',()=> addNewNote())
+addBtn.addEventListener('click',()=> addNewNote())
 
-function addNewNote(text=''){
+function addNewNote(text ='') {
     const note = document.createElement('div')
     note.classList.add('note')
     note.innerHTML = `
@@ -10,7 +10,35 @@ function addNewNote(text=''){
 <button class="edit"><i class="fas fa-edit"></i></button>
 <button class="delete"><i class="fas fa-trash-alt"></i></button>
 </div>
-    <div class="main ${text ? "" : hidden"}"></div>
-    <textarea class="${text ? "hidden" : ""}></textarea>
-    `
+    <div class="main ${text ? "" : hidden" }">
+    </div>
+    <textarea class="${text ? "hidden" : "" }">
+    </textarea>
+`
+const editBtn = note.querySelector('.edit')
+const deleteBtn = note.querySelector('.delete')
+const main = note.querySelector('.main')
+const textArea = note.querySelector('.textarea')
+
+textArea.value = text
+main.innerHTML = marked(text)
+
+
+deleteBtn.addEventListener('click',()=> {
+    note.remove()
+})
+
+editBtn.addEventListener('click',()=> {
+    main.classList.toggle('hidden')
+    testArea.classList.toggle('hidden')
+})
+
+textArea.addEventListener('input',(e)=>{
+    const {value} = e.target
+    main.innerHTML = marked(value)
+})
+
+
+
+    document.body.appendChild(note)
 }
