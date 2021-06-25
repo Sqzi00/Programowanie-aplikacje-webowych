e82147a3bc65a7f520bfb75e35c40059095f4c0f
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-06-25 23:44:12 +0200

    u

--- Projekt1/src/index.ts
+++ Projekt1/src/index.ts
@@ -20,130 +20,106 @@ class StatsApp{
         this.watchInputValues();
     }
 
-   
-   
-   
+    createInput() {
+        this.inputsContainer = document.getElementById("inputsContainer");
+        const number = +this.numberOfInputs.value;
 
-   
-   
-   
-   
+        while(this.inputsContainer?.hasChildNodes()){
+                this.inputsContainer?.removeChild(this.inputsContainer?.lastChild);
+                this.dataArray=[];
+              }
 
-   
-   
-   
-   
-   
+        for (let i = 0; i < number; i++) {
+            const newInputLabel = document.createElement("label");
+            newInputLabel.innerHTML = "Value: ";
+            newInputLabel.setAttribute("class", "data" + (i + 1));
+            this.inputsContainer?.appendChild(newInputLabel);
 
-   
-   
-   
-   
-   
+            const newInput = document.createElement("input");
+             newInput.setAttribute("type", "text");
+            newInput.setAttribute("class", "data" + (i + 1));
+            newInput.setAttribute("id","input"+(i+1));
+            this.inputsContainer?.appendChild(newInput);
 
-   
-   
-   
-   
+            const removeInputButton = document.createElement("button");
+            removeInputButton.textContent = "Remove";
+            removeInputButton.className = "data" + (i + 1);
+            this.inputsContainer?.appendChild(removeInputButton);
 
-   
+            removeInputButton.addEventListener('click', (ev:Event)=> {
 
-   
 
-   
-   
 
-   
-   
 
-   
-   
 
-   
-   
-   
 
-   
-   
-   
 
-   
-   
 
-   
-   
 
-   
-   
-   
-   
-   
 
-   
-   
-   
-   
-   
 
-   
-   
-   
 
-   
-   
-   
-   
-   
 
-   
-   
-   
-   
-   
-   
-   
 
-   
-   
-   
 
-   
-   
-   
 
-   
-   
 
-   
-   
-   
-   
-   
-   
-   
 
-   
-   
-   
 
-   
-   
-   
-   
-   
-   
 
-   
 
-   
 
-   
-   
-   
-   
 
-   
-   
-}
 
-con
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
